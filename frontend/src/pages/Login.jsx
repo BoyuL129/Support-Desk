b142e48c6a5a8cfa4f1922298bf2b7c7a022233f
Boyu Liu <boyuliu9@ad.unc.edu>
2024-06-17 01:02:50 -0500

    register (issue with proxy setting)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
 import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../features/authSlice";
 
 function Login() {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+
+  const { email, password } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -18,9 +28,14 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+    dispatch(login(userData));
   };
 
-  const { name, email, password, password2 } = formData;
   return (
     <>
       <section className="heading">
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
-import { register } from "../features/authSlice";
+import { register, reset } from "../features/authSlice";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -17,9 +18,23 @@ function Register() {
 
   const dispatch = useDispatch();
 
-  const { user, isLoading, isSuccess, message } = useSelector(
+  const navigate = useNavigate();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.auth
   );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    // redirect when logged in
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -103,7 +118,7 @@ function Register() {
             />
           </div>
           <div className="form-group">
-            <button className="btn btn-block">submit</button>
+            <button className="btn btn-block">Submit</button>
           </div>
         </form>
       </section>
